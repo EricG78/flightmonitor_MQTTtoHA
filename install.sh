166c530ea336cf7645f2970f23a57c39795ad67a
Eric Georgeaux <76625435+EricG78@users.noreply.github.com>
2021-08-25 00:59:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #Copyright © 2021 Eric Georgeaux (eric.georgeaux at gmail.com)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), 
@@ -12,28 +13,39 @@
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
 # Check the script is ran with root priviledge
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
+# Check dependancies
+dl_jq=`[ -z "$(which jq)" ] && echo "jq"`
+dl_bc=`[ -z "$(which bc)" ] && echo "bc"`
+dl_mosquitto=`[ -z "$(which mosquitto_pub)" ] && echo "mosquitto-clients"`
+apt_arg="$dl_jq $dl_bc $dl_mosquitto"
+
+if [ ${#apt_arg} -gt 2 ]; then
+        apt-get install $apt_arg
+fi
+
 scriptName="flightmonitor_MQTTtoHA"
 
 # Find bash location
 bashPath="/bin/bash"
-if [[ ! -f $bashPath ]]; then
+if [ ! -f $bashPath ]; then
         bashPath=$(which bash)
         echo "bash found by which command: $bashPath"
 fi
-if [[ -z bashPath ]]; then
+if [ -z bashPath ]; then
         echo "Error: Unable to find bash"
         exit
 fi
 
 # Check the location of the script
 scriptPath="$(pwd)/$scriptName.sh"
-if [[ ! -f $scriptPath ]]; then
+if [ ! -f $scriptPath ]; then
         echo "Error: unable to find $scriptName.sh in the current directory."
         exit
 fi
